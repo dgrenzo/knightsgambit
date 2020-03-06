76ac4ca36f36844bca695cc2ce1d2731bda4db49
davidisneato <davidgrenzowski+github@Gmail.com>
2020-03-06 13:30:20 -0500

    change background color and title

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,23 +1,14 @@
 import PIXI = require("pixi.js");
 import {Howl} from 'howler';
 
-let pixi_app : PIXI.Application;
+let pixi_app : PIXI.Application = new PIXI.Application({
+  autoResize: true,
+  backgroundColor : 0x000011,
+  view : <HTMLCanvasElement>document.getElementById('game_canvas')
+});
 
-const Init = () => {
-  pixi_app = new PIXI.Application({
-    autoResize: true,
-    backgroundColor : 0x111122,
-    view : <HTMLCanvasElement>document.getElementById('game_canvas')
-  });
-
-  let WindowResize = () => {
-    pixi_app.renderer.resize(window.innerWidth, window.innerHeight);
-  }
-  window.addEventListener('resize', WindowResize);
-  Promise.resolve().then(WindowResize);
-
-  pixi_app.renderer.plugins.interaction.on('pointerdown', (evt : PIXI.interaction.InteractionEvent) => {
-  });
+let WindowResize = () => {
+  pixi_app.renderer.resize(window.innerWidth, window.innerHeight);
 }
-
-Init();
+window.addEventListener('resize', WindowResize);
+Promise.resolve().then(WindowResize);
