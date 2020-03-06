e4ed88346baf927196353376e927d6a6612bb1fd
davidisneato <davidgrenzowski+github@Gmail.com>
2020-03-06 14:51:38 -0500

    gh-pages build

---
+++ game/board.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var COLOR = {
+    BLACK: 0x393939,
+    WHITE: 0xF0F0F0,
+};
+var Board = (function () {
+    function Board() {
+    }
+    return Board;
+}());
+exports.Board = Board;
